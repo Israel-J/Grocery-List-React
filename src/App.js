062a17b4215f12bee346f9cf44239ae1e3a46cf9
Israel Rdoriguez <israel355576@gmail.com>
2021-03-18 17:44:31 -0700

    Added Task and Tasks components.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import Header from "./components/Header";
+import Tasks from "./components/Tasks";
 
 function App() {
+  //EVENTS
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      text: "Food Shopping",
+      day: "Feb 13th at 1pm",
+      reminder: true,
+    },
+    {
+      id: 2,
+      text: "Doctors appointment",
+      day: "Feb 13th at 12pm",
+      reminder: true,
+    },
+    {
+      id: 3,
+      text: "Gym time",
+      day: "Feb 13th at 10am",
+      reminder: false,
+    },
+  ]);
+
+  // Delete Task
+
+  const deleteTask = (id) => {
+    setTasks(tasks.filter((task) => task.id !== id))
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <Header />
+      <Tasks tasks={tasks} onDelete={deleteTask} />
     </div>
   );
 }

5739e6cecdc7c979e3ae454c9da9aae6d1fbf1ed
Israel Rdoriguez <israel355576@gmail.com>
2021-03-19 19:31:07 -0700

    Made the btn open the form when clicked

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import { useState } from "react";
 import Header from "./components/Header";
 import Tasks from "./components/Tasks";
+import AddTask from "./components/AddTask";
 
 function App() {
+  //TOGGLES TO SHOW FORM
+  const [showAddTask, setShowAddTask] = useState(false)
   //EVENTS
   const [tasks, setTasks] = useState([
     {
@@ -25,16 +28,40 @@ function App() {
     },
   ]);
 
+  // ADD Task
+  const addTask = (task) => {
+    //MAKES RANDOM ID SINCE WE DONT HAVE BACKEND
+    const id = Math.floor(Math.random() * 1000) + 1;
+
+    const newTask = { id, ...task}
+    setTasks([...tasks, newTask])
+  };
+
   // Delete Task
 
   const deleteTask = (id) => {
-    setTasks(tasks.filter((task) => task.id !== id))
+    setTasks(tasks.filter((task) => task.id !== id));
+  };
+
+  // TOGGLE REMINDER
+
+  const toggleReminder = (id) => {
+    setTasks(
+      tasks.map((task) =>
+        task.id === id ? { ...task, reminder: !task.reminder } : task
+      )
+    );
   };
 
   return (
     <div className="container">
-      <Header />
-      <Tasks tasks={tasks} onDelete={deleteTask} />
+      <Header onAdd={() => setShowAddTask(!showAddTask)} showAdd={showAddTask}/>
+      {showAddTask && <AddTask onAdd={addTask} />}
+      {tasks.length > 0 ? (
+        <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />
+      ) : (
+        "No List To Show"
+      )}
     </div>
   );
 }
